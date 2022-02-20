572fb08d223896378a897cb32f82ddc063c242da
Djordje <djordje_v@hotmail.com>
2022-02-20 14:06:08 -0800

    Write contracts + test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
 
-const privateKey = fs.readFileSync(".secret").toString();
+const privateKey = fs.readFileSync(".secret").toString().trim();
 const projectId = "d6030038b9c0479e86c9f3698733ffbe";
 
 module.exports = {
@@ -12,11 +12,11 @@ module.exports = {
     mumbai: {
       // put in env variable
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
+      accounts: [`0x${privateKey}`],
     },
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
+      accounts: [`0x${privateKey}`],
     },
   },
   solidity: "0.8.4",
