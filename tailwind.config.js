92de08ce275372993e5c935c6c94a96f8ede9910
Djordje <djordje_v@hotmail.com>
2022-02-20 15:07:44 -0800

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
